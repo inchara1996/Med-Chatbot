d5b8df5cb26e82e7b6269423053c3370f5fe69dc
inchara1996 <incharanbhushan1996@gmail.com>
2026-03-14 16:35:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,36 +1,19 @@
-provider "azurerm" {
-  features {}
-}
 
-# Read existing Resource Group
-data "azurerm_resource_group" "rg" {
-  name = "Chidu-RG"
-}
 
-# Read existing Storage Account
-data "azurerm_storage_account" "storage" {
-  name                = "eval4adosa"
-  resource_group_name = data.azurerm_resource_group.rg.name
-}
 
-# Read existing Container
-data "azurerm_storage_container" "container" {
-  name                 = "123"
-  storage_account_name = data.azurerm_storage_account.storage.name
-}
 
 # Virtual Network
 resource "azurerm_virtual_network" "vnet" {
   name                = "vnet-demo"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.0.0.0/16"]
 }
 
 # Subnet 1
 resource "azurerm_subnet" "subnet1" {
   name                 = "subnet-1"
-  resource_group_name  = data.azurerm_resource_group.rg.name
+  resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
@@ -38,7 +21,7 @@ resource "azurerm_subnet" "subnet1" {
 # Subnet 2
 resource "azurerm_subnet" "subnet2" {
   name                 = "subnet-2"
-  resource_group_name  = data.azurerm_resource_group.rg.name
+  resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
 }
@@ -46,8 +29,8 @@ resource "azurerm_subnet" "subnet2" {
 # Public IP VM1
 resource "azurerm_public_ip" "pip1" {
   name                = "vm1-public-ip"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
   sku                 = "Standard"
 }
@@ -55,8 +38,8 @@ resource "azurerm_public_ip" "pip1" {
 # Public IP VM2
 resource "azurerm_public_ip" "pip2" {
   name                = "vm2-public-ip"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
   sku                 = "Standard"
 }
@@ -64,8 +47,8 @@ resource "azurerm_public_ip" "pip2" {
 # NIC 1
 resource "azurerm_network_interface" "nic1" {
   name                = "vm1-nic"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
     name                          = "internal"
@@ -78,8 +61,8 @@ resource "azurerm_network_interface" "nic1" {
 # NIC 2
 resource "azurerm_network_interface" "nic2" {
   name                = "vm2-nic"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
     name                          = "internal"
@@ -92,9 +75,9 @@ resource "azurerm_network_interface" "nic2" {
 # VM 1
 resource "azurerm_linux_virtual_machine" "vm1" {
   name                = "vm-1"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
-  size                = "Standard_B2s"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  size                = "Standard_D4as_v5"
 
   admin_username = var.admin_username
   admin_password = var.admin_password
@@ -121,9 +104,9 @@ resource "azurerm_linux_virtual_machine" "vm1" {
 # VM 2
 resource "azurerm_linux_virtual_machine" "vm2" {
   name                = "vm-2"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
-  size                = "Standard_B2s"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  size                = "Standard_D4as_v5"
 
   admin_username = var.admin_username
   admin_password = var.admin_password
