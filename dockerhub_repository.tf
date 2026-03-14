799030aae1fd3a01538d7b7a583651c7a2871cb0
inchara1996 <incharanbhushan1996@gmail.com>
2026-03-14 20:55:38 +0530

    Delete dockerhub_repository.tf

--- dockerhub_repository.tf
+++
@@ -1,7 +0,0 @@
-resource "dockerhub_repository" "repo" {
-  name        = "shivalik-docker-repo"
-  namespace   = "suraj008docker"
-  description = "Repository created using Terraform"
-
-  visibility = "public"
-}
